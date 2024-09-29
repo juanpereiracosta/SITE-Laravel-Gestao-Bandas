675a3dc7bce2526dbdcd8203bf210e20ee685e50
Juan <juanpereiracosta@gmail.com>
2024-09-29 16:54:49 +0100

    updates

--- final-project/routes/web.php
+++ final-project/routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Routing\Annotation\Route as AnnotationRoute;
+use Symfony\Component\Routing\Route as ComponentRoutingRoute;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+//Route::get('/bands', [HomeController::class, 'index'])->name('home');
+
+
+
+
 
--- final-project/storage/framework/views/ea5f01705e28dfa8cb2c827bf9115bb7.php
+++ final-project/storage/framework/views/ea5f01705e28dfa8cb2c827bf9115bb7.php
@@ -3,47 +3,61 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Título do Site</title>
+    <title>Cosmic</title>
 
     <!-- CSS do Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Reem+Kufi+Fun:wght@400..700&display=swap');
+
+        .header {
+        background-color: #000;
+        text-align: center;
+        padding: 50px 0;
+        font-family: 'Reem Kufi Fun', sans-serif;
+        }
+
+        .header h1 {
+        font-weight: bold;
+        font-size: 6rem;
+        margin: 0;
+        color: #f2b409;
+        }
+
+        .header p {
+        font-size: 1.5rem;
+        margin: 5px 0;
+        color: white;
+        }
+    </style>
 </head>
-<body>
-<!-- header do Bootstrap -->
-<div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
-    <div class="bg-dark p-4">
-        <h5 class="text-body-emphasis h4">Collapsed content</h5>
-        <span class="text-body-secondary">Toggleable via the navbar brand.</span>
-    </div>
-</div>
-<nav class="navbar navbar-dark bg-dark">
-    <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-    </div>
-</nav>
+
+</body>
+      <header class="header">
+        <h1>Cosmic</h1>
+        <p>Music is everywhere</p>
+      </header>
 
 <!-- Conteúdo da página -->
 <?php echo $__env->yieldContent('content'); ?>
 
 <!-- footer do Bootstrap -->
-<div class="container">
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <div class="col-md-4 d-flex align-items-center">
-            <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-                <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
-            </a>
-            <span class="mb-3 mb-md-0 text-body-secondary">© 2024 Company, Inc</span>
-        </div>
-
-        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-            <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
-            <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
-            <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
-        </ul>
-    </footer>
-</div>
+<style>
+    .footer {
+        background-color: black;
+        color: white;
+        font-weight: bold;
+        text-align: center;
+        padding: 10px 0;
+    }
+</style>
+<footer class="footer">
+    <a href="#" class="text-white" aria-label="Instagram">
+        <i class="fab fa-instagram"></i>
+    </a>
+    <div>&copy; 2024 Seu Nome</div>
+</footer>
 
 <!-- JS do Bootstrap -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
